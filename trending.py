005a69942db88bfbcb43e3aa23f6ed9ff6d7f843
allcryptoex <allcryptoex@gmail.com>
2025-04-29 16:16:33 -0500

    Update trending.py

--- trending.py
+++ trending.py
@@ -12,7 +12,7 @@ def app():
     # # # HOME
     st.header(' :green[Welcome to the Allen Speller!!]')
     col1, col2 = st.columns([2,3])
-    # X col1.video('simu.mp4')  
+    # col1.video('simu.mp4')  
     col2.markdown(' **The spelling practice platform for all Allen's campuses.** ')
     col2.markdown('''Allen Speller makes it easier to practice the UIL Spelling Competition.''')
     # X col2.markdown('''Aunthenticity, Honesty and an Unwavering dedication to our users are our core values MWANACHUO is geared to become the number one news outlet source in the university.''')
