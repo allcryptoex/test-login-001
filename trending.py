7cf19908f84ad780d61135ffd3a55713b7cb4993
allcryptoex <allcryptoex@gmail.com>
2025-04-30 17:01:00 -0500

    Update trending.py

--- trending.py
+++ trending.py
@@ -10,7 +10,7 @@ def app():
         container1.image('logo.png', width=100)
 
     
-    st.header(' :green[Starting started!!]')
+    st.header(' :green[Starting Practice!!]')
     col1, col2 = st.columns([2,3])
  
     col2.markdown(' **The best practice platform for UiL Spellers.** ')
